0fc138a1be20239c043668c17a05a6f258f4f1ef
snake404 <snake_404@hotmail.com>
2017-10-31 18:08:32 -0600

    backoffice adminRest and activation code

--- user_component/postgres/initdb.sh
+++ user_component/postgres/initdb.sh
@@ -4,5 +4,7 @@ psql postgres < /tmp/1_create_db.sql;
 psql postgres < /tmp/2_grant_privileges.sql;
 psql -U abanking_sco abanking_sco < /tmp/3_create_tables.sql
 psql -U abanking_sco abanking_sco < /tmp/4_create_tables.sql
-
+psql -U abanking_sco abanking_sco < /tmp/5_create_tables.sql
+psql -U abanking_sco abanking_sco < /tmp/6_create_tables.sql
+psql -U abanking_sco abanking_sco < /tmp/7_insert_backoffice_data.sql
 echo "Finish data base";
